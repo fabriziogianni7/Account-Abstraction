904e8a6a834014dc3aa44b45c01bdca91bbe79a9
Fabriziogianni7 <fabrizio@keyko.io>
2023-07-14 17:18:45 +0200

    entrypoint update

--- contracts/step-3/EntryPointStepThree.sol
+++ contracts/step-3/EntryPointStepThree.sol
@@ -16,46 +16,6 @@ contract EntryPointStepThree {
 
     constructor() {}
 
-    /**
-    check to see if wallet has enough funds
-    call executeOp on wallet
-    send eth to executor to pay gas
-    so in this case, the wallet need to deposit funds here to pay for the transactions.
-     */
-
-    function handleOp(UserOperation calldata op) public {
-        uint256 baseFee = 2100;
-        uint256 gasPrice = tx.gasprice;
-        uint256 totalGas;
-        WalletStepThree wallet = WalletStepThree(payable(op.sender));
-
-        // //check to see if wallet has enough funds
-        // //Set aside ETH from the wallet’s deposit to pay for the maximum amount of gas it might possibly use (based on the op's gas field). If the wallet doesn’t have enough, reject.
-        uint256 walletBalance = op.sender.balance;
-        uint256 depos = deposits[op.sender];
-        uint256 missingAmount = 0;
-        if (depos.add(walletBalance) < op.gasLimit) {
-            revert("wallet does not have enough funds/deposit!");
-        } else if (depos <= op.gasLimit) {
-            missingAmount = (op.gasLimit.sub(depos)).mul(gasPrice);
-        }
-
-        // call validateOp
-        uint256 validationGas = wallet.validateOp(op, missingAmount);
-        uint256 executionGas = wallet.executeOp(op);
-
-        totalGas =
-            op.gasLimit +
-            executionGas +
-            validationGas +
-            baseFee -
-            gasleft();
-        address[] memory senders = new address[](1);
-        senders[0] = op.sender;
-
-        _refundExecutor(totalGas.mul(gasPrice), senders);
-    }
-
     function handleOps(UserOperation[] calldata ops) public {
         uint256 startGas = gasleft();
         uint256 initialGasSpent= 30000000- startGas;
